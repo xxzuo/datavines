1ee73330929c8480de7641a1379aefc8587eb6be
xxzuo <1293378490@qq.com>
2024-08-03 14:40:30 +0800

    doris catalog

---
+++ datavines-connector/datavines-connector-plugins/datavines-connector-doris-catalog/src/main/java/DorisCatalogConfigBuilder.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import io.datavines.common.param.form.PluginParams;
+import io.datavines.common.param.form.Validate;
+import io.datavines.common.param.form.type.InputParam;
+import io.datavines.connector.plugin.JdbcConfigBuilder;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+public class DorisCatalogConfigBuilder extends JdbcConfigBuilder {
+
+    @Override
+    protected InputParam getPropertiesInput(boolean isEn) {
+        return getInputParam("properties",
+                isEn ? "properties" : "参数",
+                isEn ? "please enter properties,like key=value&key1=value1" : "请填入参数，格式为key=value&key1=value1", 2, null,
+                "useUnicode=true&characterEncoding=UTF-8&useSSL=false&serverTimezone=Asia/Shanghai&useInformationSchema=true");
+    }
+
+    @Override
+    protected InputParam getCatalogInput(boolean isEn) {
+        return getInputParam("catalog",
+                isEn ? "catalog" : "数据目录",
+                isEn ? "please enter catalog" : "请填入数据目录", 1,
+                Validate.newBuilder().setRequired(true).setMessage(isEn ? "please enter catalog" : "请填入数据目录").build(),
+                null);
+    }
+
+    @Override
+    protected InputParam getDatabaseInput(boolean isEn) {
+        return getInputParam("database",
+                isEn ? "database" : "数据库",
+                isEn ? "please enter database" : "请填入数据库", 1, null,
+                null);
+    }
+
+    @Override
+    protected List<PluginParams> getOtherParams(boolean isEn) {
+
+        List<PluginParams> list = new ArrayList<>();
+
+        InputParam enableExternalCatalog = getInputParam("enable_use_view",
+                isEn ? "enable.use.view" : "允许使用视图(设置为false时无法导出错误数据)",
+                isEn ? "please enter true or false" : "请填入 true 或者 false", 2, null,
+                "true");
+
+        list.add(enableExternalCatalog);
+        return list;
+    }
+}
